d08a48b9cb845fd7b8772f29184b057425669415
Gourav Khunger <gouravkhunger18@gmail.com>
2022-03-14 23:03:00 +0530

    Add default notification sound

--- app/src/main/java/com/github/gouravkhunger/quotesapp/workers/DailyQuoteWorker.kt
+++ app/src/main/java/com/github/gouravkhunger/quotesapp/workers/DailyQuoteWorker.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.media.RingtoneManager
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -78,6 +79,9 @@ class DailyQuoteWorker(
                     )
                     .setPriority(NotificationCompat.PRIORITY_HIGH)
                     .setContentIntent(pendingIntent)
+                    .setSound(
+                        RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                    )
 
                 // create notification channels for SDK >= SDK 26
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
